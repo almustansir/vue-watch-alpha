cc0bd136b7ef3421825530a3f506356642fb0486
Samit_08 <almustansir465@gmail.com>
2022-05-29 04:44:08 +0600

    stopWatch added

--- src/components/StopWatchComp.vue
+++ src/components/StopWatchComp.vue
@@ -1,11 +1,61 @@
 <template>
   <h3>Stop Watch</h3>
-  <p>Stop Watch here...</p>
+  <p>
+    <button @click="start">Start</button> || <button @click="stop">Stop</button> || <button @click="reset">Reset</button>
+  </p>
+  <p>{{ hours }}:{{ minutes }}:{{ seconds }}</p>
 </template>
 
 <script>
 export default {
-    name: "StopWatchComp"
+    name: "StopWatchComp",
+    data() {
+      return {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        intervalId: null
+      }
+    },
+
+    methods: {
+      start() {
+        // defines 'intervalId'
+        clearInterval(this.intervalId)
+
+        // increment starts
+        this.intervalId = setInterval(()=>{
+
+          // seconds increment
+          this.seconds = this.seconds + 1
+
+          // minutes increment
+          if (this.seconds == 60) {
+            this.seconds = 0
+            this.minutes = this.minutes + 1
+          }
+
+          // hours increment
+          if (this.minutes == 60) {
+            this.minutes = 0
+            this.hours = this.hours + 1
+          }
+        }, 1000)
+      },
+      stop() {
+        // interval stops
+        clearInterval(this.intervalId)
+      },
+      reset() {
+        // timer resets
+        this.hours = 0
+        this.minutes = 0
+        this.seconds = 0
+
+        // increment stops
+        clearInterval(this.intervalId)
+      }
+    }
 }
 </script>
 
--- src/components/TimerComp.vue
+++ src/components/TimerComp.vue
@@ -1,11 +1,46 @@
 <template>
   <h3>Timer</h3>
-  <p>Timer here...</p>
+  <p>
+    <input type="text" placeholder="hours" v-model="hours"><br>
+    <input type="text" placeholder="minutes" v-model="minutes"><br>
+    <input type="text" placeholder="seconds" v-model="seconds"><br>
+    <button @click="start">Start</button>
+  </p>
+  <p>
+    <span v-if="hours != null">{{ hours }}</span><span v-else>0</span>
+    : <span v-if="minutes != null">{{ minutes }}</span><span v-else>0</span>
+    : <span v-if="seconds != null">{{ seconds }}</span><span v-else>0</span>
+  </p>
 </template>
 
 <script>
 export default {
-    name: 'TimerComp'
+    name: 'TimerComp',
+    data() {
+      return {
+        hours: null,
+        minutes: null,
+        seconds: null,
+        intervalId: null
+      }
+    },
+    methods: {
+      start() {
+        clearInterval(this.intervalId)
+        setInterval(() => {
+          this.seconds = this.seconds - 1
+          if (this.seconds == 0) {
+            console.log('done')
+            stop()
+            clearInterval(this.intervalId)
+          }
+        }, 1000)
+      },
+      stop() {
+        clearInterval(this.intervalId)
+      }
+    }
+
 }
 </script>
 
